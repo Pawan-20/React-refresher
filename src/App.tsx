09d56624ba26c57e9bf53537f07acf40602972b1
pawan-20 <pawankumar85540@gmail.com>
2025-04-28 17:00:12 +0530

    feat: added jsonebserver and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
 import "./index.css";
+import LearnPage from "./LearnPage";
+import LearnChildPage from "./LearnChildPage";
+import DataVerificationPage from "./DataVerificationPage";
 export default function App() {
-  return <div className="w-full h-screen bg-teal-50 p-10 ">App</div>;
+  return (
+    <div className="p-4 bg-teal-500">
+      <Routes>
+        <Route index element={<HomePage></HomePage>}></Route>
+        <Route path="/learn" element={<LearnPage></LearnPage>}></Route>
+        <Route
+          path="/learn-child"
+          element={<LearnChildPage></LearnChildPage>}
+        ></Route>
+        <Route
+          path="/data-verification"
+          element={<DataVerificationPage></DataVerificationPage>}
+        ></Route>
+        <Route path="*" element={<HomePage></HomePage>}></Route>
+      </Routes>
+    </div>
+  );
 }
